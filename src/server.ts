b94d5397be20d1168a1e06300d615cc0c7318b95
Serabass <serabass565@gmail.com>
2021-05-22 19:10:50 +0600

    upd

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+const bs = require('browser-sync');
 
 // Constants
 const PORT = 8080;
@@ -9,8 +10,16 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req: any, res: any) => {
-  res.send('NIC4\n');
+  res.send('1111\n');
 });
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
+bs.init({
+  // The hostname is the name of your service in docker-compose.yml.
+  // The port is what's defined in your Dockerfile.
+  proxy: "localhost:8080",
+  notify: false,
+  // Do not open browser on start
+  open: false
+})
